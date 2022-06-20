e5bcd844621c35f94fb3be4a485a94b2b1bd1e32
Stephen Pinkerton <S.pinkerton@zohomail.com>
2022-06-20 00:05:09 -0400

    Full hour coding completed

--- Day-0.py
+++ Day-0.py
@@ -12,3 +12,16 @@ else:  ## otherwise this block runs
     Weekly_pay = float(Hourly_rate) * float(Hours_worked)
     print("\nYour weekly pay is equal to: ", Weekly_pay)
     print("\n")
+
+## This part of the code will just tell you what generation your belong to...even if you don't like it. :)
+Year_born = input("What year were you born: ")
+if int(Year_born) < 1946:
+    print("You are a Traditionalist or part fo the Silent Generation. \n")
+elif int(Year_born) > 1945 and int(Year_born) < 1965:
+    print("You are part of the Baby Boomers Generation. \n")
+elif int(Year_born) >= 1965 and int(Year_born) < 1977:
+    print("You are part of Generation X. \n")
+elif int(Year_born) >= 1977 and int(Year_born) < 1996:
+    print("You are part of the Millennials or Gen Y. \n")
+elif int(Year_born) >= 1996 and int(Year_born) < 2015:
+    print("You are part of the Gen Z, iGen, or Centennials Generation. \n")
